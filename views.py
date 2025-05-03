88e72a63b262b076d46fd010b70325045d2c4ede
kellyjames2 <135234517+kellyjames2@users.noreply.github.com>
2025-05-03 17:18:30 +0000

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
-from . import db
+from models import Note
+from __init__ import db
 import json
 
 views = Blueprint('views', __name__)
